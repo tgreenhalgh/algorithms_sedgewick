afd08954f1dee766dc647650b82b83b2cf756b26
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-01-02 21:17:28 -0800

    adds more tests

--- union-find/quickFindEager.spec.js
+++ union-find/quickFindEager.spec.js
@@ -10,5 +10,15 @@ describe('quick find', () => {
       const UF = new helpers.QuickFindUF(5);
       expect(typeof UF).toBe('object');
     });
+
+    it('should have the correct size', () => {
+      const UF = new helpers.QuickFindUF(5);
+      expect(UF.QuickFindUF().length).toBe(5);
+    });
+
+    it('should check if two objects are connected', () => {
+      const UF = new helpers.QuickFindUF(5);
+      expect(UF.connected(2, 3)).toBe(true);
+    });
   });
 });
